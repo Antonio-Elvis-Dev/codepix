9a6bfee9f5b33053d2d4bb90e955079119f2d24a
antonio-elvis-dev <githubantonioelvis@gmail.com>
2023-10-19 01:06:15 -0300

    update aula 1

--- domain/model/account.go
+++ domain/model/account.go
@@ -1,6 +1,17 @@
 package model
+import "github.com/asaskevich/govalidator"
 
 type Acconunt struct {
 	Base      `valid:"required"`
 	OwnerName string `json:"owner_name" valid:"notnull"`
+	Bank *Bank `valid:"-"`
+	Number string `json:"number" valid:"notnull"`
+}
+
+func (account *Acconunt) isValid() error{
+	_, err := govalidator.ValidateStruct(account)
+	if err != nil{
+		return err
+	}
+	return nil
 }
